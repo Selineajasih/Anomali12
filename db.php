3b76c9bc730dd064c636f4a4df5f79be6ae3b90d
unknown <xvivydiva@gmail.com>
2025-05-14 10:42:12 +0700

    updt

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "pecintasylus";
+$dbname = "db_jma";
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
--- index.php
+++ index.php
@@ -64,8 +64,7 @@ Our cleaners are fully trained, police-checked, and show up on time (with all th
       <div class="w-full px-10 py-10 md:px-20 lg:px-40 flex justify-center items-center">
         <img src="assets/7426287.svg" alt="cleaner-image" class="">
       </div>
-    </section>
-9
+    </section>`
     <!-- Services -->
     <section class="justify-center w-full">
       <h2 class="text-xl md:text-2xl lg:text-3xl font-semibold text-cyan-700 mb-4 w-full text-center">Our Service</h2>
